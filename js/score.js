b54f5a88b5ee60084d28f8d012b34ebf60018005
cesar4206 <cesar4206@live.com.mx>
2019-04-03 18:51:36 -0600

    Agregando Menu

---
+++ js/score.js
@@ -0,0 +1,7 @@
+const CANVAS_BORDER_COLOUR = 'blue';
+const CANVAS_BACKGROUND_COLOUR = "blue";
+
+// Get the canvas element
+const gameCanvas = document.getElementById("Score");
+// Return a two dimensional drawing context
+const ctx = gameCanvas.getContext("2d");
