9647ddeba14eb5ec8a246beddb9221042d6682d4
raulgg1996 <31137192+raulgg1996@users.noreply.github.com>
2019-04-12 13:21:27 -0500

    Update entorno.js

--- src/entorno.js
+++ src/entorno.js
@@ -9,8 +9,46 @@ var h = document.getElementById("myCanvas").offsetHeight;
 var canonImageLoaded = false,canonImage2Loaded = false, torreImageLoaded = false, bombaImageLoaded = false,bombaImage2Loaded = false, fondoImageLoaded = false;
 //declaracion de variables temporales incluyendo los dos scores 
 var objectSizes = 20;
+var vi=50.0;
+var angi=45.0;
+var g=9.8;
+var vy=0;
+var vx=0;
+var t=0;
+var h=0;
+var ai=0;
 var score1 = 0;
 var score2 = 0;
+var b1x=0;
+var b1y=0;
+var b2x=0;
+var b2y=0;
+var fx=0;
+var fy=0;
+var t1x=0;
+var t2x=0;
+var t3x=0;
+var t4x=0;
+var t5x=0;
+var t6x=0;
+var t7x=0;
+var t8x=0;
+var t9x=0;
+var t10x=0;
+var t1y=0;
+var t2y=0;
+var t3y=0;
+var t4y=0;
+var t5y=0;
+var t6y=0;
+var t7y=0;
+var t8y=0;
+var t9y=0;
+var t10y=0;
+var c1x=0;
+var c1y=0;
+var c2x=0;
+var c2y=0;
 //cargamos la imagen del cañon 
 //cañon PLAYER 1
 var canonImage = new Image();
@@ -86,100 +124,132 @@ function update() {
     var fondo = new Object;
     fondo.x = 0;
     fondo.y = 0;
+    fx=fondo.x;
+    fy=fondo.y;
     //////////////////////////////////////////
     var Torre1 = new Object;
     Torre1.x = 0;
+    t1x=Torre1.x;
     Torre1.y = Math.random()*400;
+    t1y=Torre1.y;
     if(Torre1.y < 150)
     {
-        Torre1.y=150;    
+        Torre1.y=150; 
+        t1y=Torre1.y;   
     }
     console.log("Torre1: "+Torre1.y);
     //////////////////////////////////////////
     var Torre2 = new Object;
     Torre2.x = 100;
+    t2x=Torre2.x;
     Torre2.y = Math.random()*400;
+    t2y=Torre2.y;
     console.log("Torre2: "+Torre2.y );
     if(Torre2.y < 150)
     {
-        Torre2.y=150;    
+        Torre2.y=150;
+        t2y=Torre2.y;   
     }
     //////////////////////////////////////////
     var Torre3 = new Object;
     Torre3.x = 200;
+    t3x=Torre3.x;
     Torre3.y = Math.random()*400;
+    t3y=Torre3.y;
     if(Torre3.y < 150)
     {
-        Torre3.y=150;    
+        Torre3.y=150;
+        t3y=Torre3.y;    
     }
     console.log("Torre3: "+Torre3.y );
 
     //////////////////////////////////////////
     var Torre4 = new Object;
     Torre4.x = 300;
+    t4x=Torre4.x;
     Torre4.y = Math.random()*400;
+    t4y=Torre4.y;
     if(Torre4.y < 150)
     {
-        Torre4.y=150;    
+        Torre4.y=150; 
+        t4y=Torre4.y;   
     }
     console.log("Torre4: "+Torre4.y );
 
     //////////////////////////////////////////
     var Torre5 = new Object;
     Torre5.x = 400;
+    t5x=Torre5.x;
     Torre5.y = Math.random()*400;
+    t5y=Torre5.y;
     if(Torre5.y < 150)
     {
-        Torre5.y=150;    
+        Torre5.y=150;
+        t5y=Torre5.y;    
     }
     console.log("Torre5: "+Torre5.y );
 
     //////////////////////////////////////////
     var Torre6 = new Object;
     Torre6.x = 500;
+    t6x=Torre6.x;
     Torre6.y = Math.random()*400;
+    t6y=Torre6.y;
     if(Torre6.y < 150)
     {
-        Torre6.y=150;    
+        Torre6.y=150; 
+        t6y=Torre6.y;   
     }
     console.log("Torre6: "+Torre6.y );
 
     //////////////////////////////////////////
     var Torre7 = new Object;
     Torre7.x = 600;
+    t7x=Torre7.x;
     Torre7.y = Math.random()*400;
+    t7y=Torre7.y;
     if(Torre7.y < 150)
     {
-        Torre7.y=150;    
+        Torre7.y=150; 
+        t7y=Torre7.y;   
     }
     console.log("Torre7: "+Torre7.y );
 
     //////////////////////////////////////////
     var Torre8 = new Object;
     Torre8.x = 700;
+    t8x=Torre8.x;
     Torre8.y = Math.random()*400;
+    t8y=Torre8.y;
     if(Torre8.y < 150)
     {
-        Torre8.y=150;    
+        Torre8.y=150;
+        t8y=Torre8.y;    
     }
     console.log("Torre8: "+Torre8.y );
 
     //////////////////////////////////////////
     var Torre9 = new Object;
     Torre9.x = 800;
+    t9x=Torre9.x;
     Torre9.y = Math.random()*400;
+    t9y=Torre9.y;
     if(Torre9.y < 150)
     {
-        Torre9.y=150;    
+        Torre9.y=150;
+        t9y=Torre9.y;    
     }
     console.log("Torre9: "+Torre9.y );
     //////////////////////////////////////////
     var Torre10 = new Object;
     Torre10.x = 900;
+    t10x=Torre10.x;
     Torre10.y = Math.random()*400;
+    t10y=Torre10.y;
     if(Torre10.y < 150)
     {
-        Torre10.y=150;    
+        Torre10.y=150; 
+        t10y=Torre10.y;   
     }
     console.log("Torre10: "+Torre10.y );
     /////////////////////////////////////////////////////////
@@ -199,9 +269,13 @@ function update() {
     var canon2 = new Object;
     canon2.x = 0;//0
     canon2.y = 0;//90
+    //c2x=canon2.x;
+    //c2y=canon2.y;
     var bomba2 = new Object;
     bomba2.bx = 0;
     bomba2.by = 0;
+    //b2x=bomba2.bx;
+    //b2y=bomba2.by;
 //vamos a selecionar una torre al azar para el cañon 1
     var azar=Math.floor(Math.random(0,5)* 4) + 1;
    // console.log("Azar: "+azar);
@@ -209,24 +283,36 @@ if(azar==1)
 {
  canon2.x=Torre1.x-60;
  canon2.y=Torre1.y-100;
+ c2x=canon2.x;
+ c2y=canon2.y;
  bomba2.bx = Torre1.x+60;
  bomba2.by = Torre1.y-50;
+ b2x=bomba2.bx;
+ b2y=bomba2.by;
  //console.log(canon2.y);
 }
 if(azar==2)
 {
  canon2.x=Torre2.x-40;
  canon2.y=Torre2.y-80;
+ c2x=canon2.x;
+ c2y=canon2.y;
  bomba2.bx = Torre2.x+90;
  bomba2.by = Torre2.y-30;
+ b2x=bomba2.bx;
+ b2y=bomba2.by;
  //console.log(canon2.y);
 }
 if(azar==3)
 {
  canon2.x=Torre3.x-40;
  canon2.y=Torre3.y-80;
+ c2x=canon2.x;
+ c2y=canon2.y;
  bomba2.bx = Torre3.x+90;
  bomba2.by = Torre3.y-30;
+ b2x=bomba2.bx;
+ b2y=bomba2.by;
  //console.log(canon2.y);
 
 }
@@ -234,21 +320,30 @@ if(azar==4)
 {
  canon2.x=Torre4.x-40;
  canon2.y=Torre4.y-80;
+ c2x=canon2.x;
+ c2y=canon2.y;
  bomba2.bx = Torre4.x+90;
  bomba2.by = Torre4.y-30;
+ b2x=bomba2.bx;
+ b2y=bomba2.by;
  //console.log(canon2.y);
 }
 ctx.drawImage(canonImage2, canon2.x, canon2.y);
-ctx.drawImage(bombaImage2, bomba2.bx, bomba2.by);
+//ctx.drawImage(bombaImage2, bomba2.bx, bomba2.by);
 
 ///////////////////////////////////CAÑON 1 ///////////////////////////
 var canon1 = new Object;
 var bomba = new Object;
 bomba.bx = 0;
 bomba.by = 0;
+b1x=bomba.bx;
+b1y=bomba.by;
 
 canon1.x = 0;//250
 canon1.y = 0;//200
+c1x=canon1.x;
+c1y=canon1.y;
+
 var azar2=Math.floor(Math.random(0,5)* 4) + 1;
 //console.log("Azar2: "+azar2)
 
@@ -256,8 +351,12 @@ if(azar2==1)
 {
  canon1.x=Torre7.x-40;
  canon1.y=Torre7.y-80;
+ c1x=canon1.x;
+ c1y=canon1.y;
  bomba.bx = Torre7.x+9;
  bomba.by = Torre7.y-30;
+ b1x=bomba.bx;
+b1y=bomba.by;
  //console.log("Torre7 Y:" + canon1.y);
  //console.log("Torre7 X:" + canon1.x);
 
@@ -266,8 +365,12 @@ if(azar2==2)
 {
  canon1.x=Torre8.x-40;
  canon1.y=Torre8.y-80;
+ c1x=canon1.x;
+ c1y=canon1.y;
  bomba.bx = Torre8.x+9;
  bomba.by = Torre8.y-30;
+ b1x=bomba.bx;
+b1y=bomba.by;
  //console.log("Torre8 Y:" + canon1.y);
  //console.log("Torre8 X:" + canon1.x);
 
@@ -276,8 +379,12 @@ if(azar2==3)
 {
  canon1.x=Torre9.x-40;
  canon1.y=Torre9.y-80;
+ c1x=canon1.x;
+ c1y=canon1.y;
  bomba.bx = Torre9.x+9;
  bomba.by = Torre9.y-30;
+ b1x=bomba.bx;
+b1y=bomba.by;
  //console.log("Torre9 y:" + canon1.y);
  //console.log("Torre9 X:" + canon1.x);
 
@@ -287,13 +394,17 @@ if(azar2==4)
 {
  canon1.x=Torre10.x-40;
  canon1.y=Torre10.y-80;
+ c1x=canon1.x;
+ c1y=canon1.y;
  bomba.bx = Torre10.x+8; 
  bomba.by = Torre10.y-30;
+ b1x=bomba.bx;
+b1y=bomba.by;
  //console.log("Torre10 X:" + canon1.x);
  //console.log("Torre10 Y:" + canon1.y);
 }
 ctx.drawImage(canonImage, canon1.x, canon1.y);
-ctx.drawImage(bombaImage, bomba.bx, bomba.by);
+//ctx.drawImage(bombaImage, bomba.bx, bomba.by);
 
     //////////////////////////////////////////////////
     //generador de fondo¡¡
@@ -341,3 +452,39 @@ function board() {
     ctx.fillStyle = "rgba(255, 255, 255, 1)";
     ctx.fillText("SCORE #1:" + score, w - 1200, h - 390);
 }
+
+function init(){
+    //alert("llego a init");
+     
+    setInterval('run()',500);
+
+    }
+
+    function run(){
+        //alert("run");
+        //b2x=b2x+5;
+        ctx.clearRect(0, 0, w, h);
+        ctx.drawImage(fondoImage, fx, fy);
+        ctx.drawImage(torreImage, t1x, t1y);
+        ctx.drawImage(torreImage, t2x, t2y);
+        ctx.drawImage(torreImage, t3x, t3y);
+        ctx.drawImage(torreImage, t4x, t4y);
+        ctx.drawImage(torreImage, t5x, t5y);
+        ctx.drawImage(torreImage, t6x, t6y);
+        ctx.drawImage(torreImage, t7x, t7y);
+        ctx.drawImage(torreImage, t8x, t8y);
+        ctx.drawImage(torreImage, t9x, t9y);
+        ctx.drawImage(torreImage, t10x, t10y);
+        ctx.drawImage(canonImage2, c2x, c2y);
+        //ctx.drawImage(bombaImage2, b2x, b2y);
+        ctx.drawImage(canonImage, c1x, c1y);
+
+        //Aqui se crean todas las formulas para la obtencion de coordenadas x,y para tiro parabolico.
+        /*gra=((parseFloat(angi)*Math.PI)/180); */
+
+       b2x=b2x+5;
+       b1x=b1x-5;
+
+         ctx.drawImage(bombaImage2, b2x, b2y);
+         ctx.drawImage(bombaImage, b1x, b1y);
+    }
